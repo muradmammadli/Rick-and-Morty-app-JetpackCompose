8c6e0cbf1dade498934305e3eb4771fce4bd8a8d
Murad <murad.mammadli@yahoo.com>
2023-09-19 17:29:50 +0400

    *

---
+++ app/src/main/java/com/az/rickandmortyapp/presentation/screens/MovieRowItem.kt
@@ -0,0 +1,15 @@
+package com.az.rickandmortyapp.presentation.screens
+
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.az.rickandmortyapp.data.model.Character
+
+@Composable
+fun MovieRowItem(character: Character){
+    Card(
+
+    ) {
+
+    }
+}
